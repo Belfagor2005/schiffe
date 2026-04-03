c9057298cca4e7b8427862f857ea43801939af56
Your Name <you@example.com>
2026-04-03 17:28:15 +0000

    Adapt repository layout and template files

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import setup_translate
+
+pkg = 'Extensions.Schiffe'
+setup(name='enigma2-plugin-extensions-schiffe',
+       version='3.0',
+       description='Schiffe Battleship Game FHD Modded by Lululla',
+       package_dir={pkg: 'Schiffe'},
+       packages=[pkg],
+       package_data={pkg: ['images/*.png', '*.png', '*.xml', 'locale/*/LC_MESSAGES/*.mo', 'Schiffe.png', 'pic/*.png', 'pic/*.jpg']},
+       cmdclass=setup_translate.cmdclass,  # for translation
+      )
